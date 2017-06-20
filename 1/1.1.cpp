83ef01dc4457696231597999322dea051ad770ec
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2017-06-19 22:41:17 -0600

    Add 1.2 - Check Permutation

--- 1/1.1.cpp
+++ 1/1.1.cpp
@@ -13,103 +13,103 @@ using namespace std;
 
 bool is_unique_no_structures(const string &str)
 {
-	// Do a comparison in place.
-	// Uses O(n) space but is O(n^2) time.
-	for(size_t i = 0; i < str.length(); i++)
-	{
-		for(size_t j = str.length() - 1; j >= i; i--)
-		{
-			if(str[i] == str[j])
-			{
-				return false;
-			}
-		}
-	}
+    // Do a comparison in place.
+    // Uses O(n) space but is O(n^2) time.
+    for(size_t i = 0; i < str.length(); i++)
+    {
+        for(size_t j = str.length() - 1; j >= i; i--)
+        {
+            if(str[i] == str[j])
+            {
+                return false;
+            }
+        }
+    }
 
-	return true;
+    return true;
 }
 
 bool is_unique_optimized(const string &str)
 {
-	// Use a bitset since we don't need to count beyond 1 for each entry.
-	// Still acts like a hash table, but reduces space by a factor of 8.
-	// O(n) space, O(n) time
-	bitset<256> bits(0);
+    // Use a bitset since we don't need to count beyond 1 for each entry.
+    // Still acts like a hash table, but reduces space by a factor of 8.
+    // O(n) space, O(n) time
+    bitset<256> bits(0);
 
-	for(size_t i = 0; i < str.length(); i++)
-	{
-		int entry = str[i];
-		if(bits.test(entry) > 0)
-		{
-			return false;
-		}
-		bits.set(entry);
-	}
+    for(size_t i = 0; i < str.length(); i++)
+    {
+        int entry = str[i];
+        if(bits.test(entry) > 0)
+        {
+            return false;
+        }
+        bits.set(entry);
+    }
 
-	return true;
+    return true;
 }
 
 bool is_unique(const string &str)
 {
-	// The integer array acts like a hash table.
-	// Keep a counter at each array entry.
-	// The character casted to int acts as the key.
-	// O(n) space, O(n) time
-	int character_counter[256] = { 0 };
+    // The integer array acts like a hash table.
+    // Keep a counter at each array entry.
+    // The character casted to int acts as the key.
+    // O(n) space, O(n) time
+    int character_counter[256] = { 0 };
 
-	for(size_t i = 0; i < str.length(); i++)
-	{
-		int entry = str[i];
-		if(character_counter[entry]++ > 1)
-		{
-			return false;
-		}
-	}
+    for(size_t i = 0; i < str.length(); i++)
+    {
+        int entry = str[i];
+        if(character_counter[entry]++ > 1)
+        {
+            return false;
+        }
+    }
 
-	return true;
+    return true;
 }
 
 void print(string str, bool unique)
 {
-	if(unique)
-	{
-		cout << str + " is unique!";
-	}
-	else
-	{
-		cout << str + " is NOT unique!";
-	}
-	cout << endl;
+    if(unique)
+    {
+        cout << str + " is unique!";
+    }
+    else
+    {
+        cout << str + " is NOT unique!";
+    }
+    cout << endl;
 }
 
 int main() {
-	string test1 = "hello!";
-	string test2 = "123456";
-	string test3 = "blockchain";
-	string test4 = " ";
-	string test5 = "square";
-	string test6 = "github";
+    string test1 = "hello!";
+    string test2 = "123456";
+    string test3 = "blockchain";
+    string test4 = " ";
+    string test5 = "square";
+    string test6 = "github";
 
-	print(test1, is_unique(test1));
-	print(test2, is_unique(test2));
-	print(test3, is_unique(test3));
-	print(test4, is_unique(test4));
-	print(test5, is_unique(test5));
-	print(test6, is_unique(test6));
+    print(test1, is_unique(test1));
+    print(test2, is_unique(test2));
+    print(test3, is_unique(test3));
+    print(test4, is_unique(test4));
+    print(test5, is_unique(test5));
+    print(test6, is_unique(test6));
 
-	print(test1, is_unique_optimized(test1));
-	print(test2, is_unique_optimized(test2));
-	print(test3, is_unique_optimized(test3));
-	print(test4, is_unique_optimized(test4));
-	print(test5, is_unique_optimized(test5));
-	print(test6, is_unique_optimized(test6));
+    print(test1, is_unique_optimized(test1));
+    print(test2, is_unique_optimized(test2));
+    print(test3, is_unique_optimized(test3));
+    print(test4, is_unique_optimized(test4));
+    print(test5, is_unique_optimized(test5));
+    print(test6, is_unique_optimized(test6));
 
-	print(test1, is_unique_no_structures(test1));
-	print(test2, is_unique_no_structures(test2));
-	print(test3, is_unique_no_structures(test3));
-	print(test4, is_unique_no_structures(test4));
-	print(test5, is_unique_no_structures(test5));
-	print(test6, is_unique_no_structures(test6));
+    print(test1, is_unique_no_structures(test1));
+    print(test2, is_unique_no_structures(test2));
+    print(test3, is_unique_no_structures(test3));
+    print(test4, is_unique_no_structures(test4));
+    print(test5, is_unique_no_structures(test5));
+    print(test6, is_unique_no_structures(test6));
 
-	return 0;
+    return 0;
 }
---
+++ 1/1.2.cpp
@@ -0,0 +1,86 @@
+//============================================================================
+// Name        : 1.2.cpp
+// Author      : Matthew Sembinelli
+// Version     :
+// Copyright   : Copyright 2017
+// Description : 1.2 - check_permutation
+//============================================================================
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+bool check_permutation_char_count(string &str1, string &str2)
+{
+    if(str1.length() != str2.length())
+    {
+        return false;
+    }
+
+    short count_chars[255] = { 0 };
+    for(size_t i = 0; i < str1.length(); i++)
+    {
+        count_chars[(int)str1[i]]++;
+        count_chars[(int)str2[i]]--;
+    }
+
+    for(size_t i = 0; i < sizeof(count_chars)/sizeof(short); i++)
+    {
+        if(count_chars[i] != 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool check_permutation(string &str1, string &str2)
+{
+    if(str1.length() != str2.length())
+    {
+        return false;
+    }
+
+    sort(str1.begin(), str1.end());
+    sort(str2.begin(), str2.end());
+    for(size_t i = 0; i < str1.length(); i++)
+    {
+        if(str1[i] != str2[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void print(const string &str1, const string &str2, bool permutation)
+{
+    if(permutation)
+    {
+        cout << str1 + " is a permutation of "+ str2;
+    }
+    else
+    {
+        cout << str1 + " is NOT a permutation of "+ str2;
+    }
+    cout << endl;
+}
+
+int main() {
+    string string_list[] = { "123456", "314265", "string", "gnrsti", "hello", "hell0" };
+
+    for(size_t i = 0; i < 6; i+=2)
+    {
+        print(string_list[i], string_list[i+1], check_permutation(string_list[i], string_list[i+1]));
+    }
+
+    for(size_t i = 0; i < 6; i+=2)
+    {
+        print(string_list[i], string_list[i+1], check_permutation_char_count(string_list[i], string_list[i+1]));
+    }
+
+    return 0;
+}
